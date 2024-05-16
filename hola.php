7346d1622f3eb77fb2622876a96295ed985358cb
Christian <christianmartininfantes@iestorredelrey.es>
2024-05-16 21:20:53 +0200

    Ups... este commit está mal.

--- hola.php
+++ hola.php
@@ -1,7 +1,7 @@
 <?php
 //christian martín infantes
 
-// El nombre por defecto es Mundo
+// Este comentario está mal y hay que borrarlo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
 </php>
