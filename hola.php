e1eb39b075882ac025a5e57e7bf05e82cc228d4b
Christian <christianmartininfantes@iestorredelrey.es>
2024-05-16 21:21:07 +0200

    Revert "Ups... este commit está mal."
    This reverts commit 7346d1622f3eb77fb2622876a96295ed985358cb.

--- hola.php
+++ hola.php
@@ -1,7 +1,7 @@
 <?php
 //christian martín infantes
 
-// Este comentario está mal y hay que borrarlo
+// El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
 </php>
